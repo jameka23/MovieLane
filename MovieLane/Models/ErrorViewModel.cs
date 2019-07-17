8d71940017c89fb3bf6dab252d579a01e2f818d2
Jameka Echols <jameka.echols@gmail.com>
2019-07-17 09:52:53 -0500

    starting over

---
+++ MovieLane/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieLane.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
